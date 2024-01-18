7a103016f344408bff732310271a7922323cadfd
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-01-18 21:27:18 +0530

    update code

---
+++ 2-Pyramids/SquarePyramid.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+/*
+ 
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ 
+ */
+
+
+
+var n = 5
+var row = 0
+var nst = n
+while(row < n) {
+    
+    var cst = 0
+    while(cst < nst){
+        print("*", terminator: " ")
+        cst += 1
+    }
+    print("\n")
+    row += 1
+}
