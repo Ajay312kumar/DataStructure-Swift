bb9416cedf332f76b13acd1098367b84cae270ca
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-01-18 21:57:49 +0530

    LeftupperTriangle

---
+++ 2-Pyramids/LeftUpperTriangle.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+/*
+ 
+ * * * * *
+ * * * *
+ * * *
+ * *
+ * 
+ 
+ */
+
+
+var n = 5
+var row = 1
+var nst = 5
+
+while row <= n {
+    
+    var nsp = 1
+    while nsp <= nst {
+        
+        print("*", terminator: " ")
+        nsp += 1
+        
+    }
+    print()
+    row += 1
+    nst -= 1
+    
+}
+
