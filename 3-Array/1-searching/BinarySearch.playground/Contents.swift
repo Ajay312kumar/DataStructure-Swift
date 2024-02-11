57bcedd50d42505c3558250ed4a789bf54c74560
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-02-11 10:02:58 +0530

    binary search

---
+++ 3-Array/1-searching/BinarySearch.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+
+
+
+var array = [2,5,6,8,10,11,13,15]
+var number = 5
+
+
+let num = binarySearch(array, number)
+print(num)
+
+
+func binarySearch(_ array:[Int], _ number: Int) -> Int{
+    
+    
+    var low = 0
+    var high = array.count - 1
+    while low < high {
+        
+        var mid = (low + high) / 2
+        
+        if number == array[mid]{
+            return mid
+        }
+        
+        if number < array[mid] {
+            
+            high = mid - 1
+            
+        }else if number > array[mid]{
+            
+            low = mid + 1
+            
+        }
+      
+    }
+    return -1
+}
+  
